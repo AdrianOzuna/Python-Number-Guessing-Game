7503cffd2ad530029ba72b72d2fdba158c26f925
Adrian <adrianozuna133@outlook.com>
2020-10-11 02:33:49 -0400

    Now users can select a desired difficulty

--- functions.py
+++ functions.py
@@ -1,13 +1,28 @@
 import random
 import math
+import sys
 
 def startGame():
-    lower = int(input("Insert lower digit:"))
-    upper = int(input("insert upper digit:"))
+
+    lower = 1
+    upper = 0
+
+    selectedDifficulty = input("Select a difficulty [easy, medium, hard]")
+
+    if selectedDifficulty == "easy":
+        upper = 5
+    elif selectedDifficulty == "medium":
+        upper = 15
+    elif selectedDifficulty == "hard":
+        upper = 30
+    else:
+        sys.exit("I couldn't understand that!")
+
     numberWithinRange = random.randint(lower,upper)
     userGuess = 0
 
     while (userGuess != numberWithinRange):
+        print("You selected the", selectedDifficulty, "Guess a number between", lower, "and", upper)
         userGuess = int(input("Your guess is: ")) 
         if userGuess < numberWithinRange:
             print("Too Low")
